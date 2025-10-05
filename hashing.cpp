da192e4bb99b719e8823efa7268f7c35e6abea9f
lgbro16 <glakshay711@gmail.com>
2025-10-05 19:20:43 +0530

    update changes

--- hashing.cpp
+++ hashing.cpp
@@ -1,5 +1,31 @@
-#include<bits/stdc++.h>
-using namespace std ;
-int main(){
-return 0;
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // Hashing is used to find the number of occurences of a specific digit or alphabet in a string or number..
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter value" << endl;
+        cin >> arr[i];
+    }
+    int hash[13] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]]++;
+    }
+    int q;
+    cout << "Enter the number of test values" << endl;
+    cin >> q;
+    int number;
+    while (q--)
+    {
+        cout << "Enter the number" << endl;
+        cin >> number;
+        cout << hash[number]<<endl;
+    }
+
+    return 0;
 }
