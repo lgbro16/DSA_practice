4c0002ef24c5fa68a4de61d2180754a591abfba7
lgbro16 <glakshay711@gmail.com>
2025-10-25 19:40:59 +0530

    learning to sort

--- sorting.cpp
+++ sorting.cpp
@@ -1,6 +1,31 @@
-#include<bits/stdc++.h>
-using namespace std ;
-int main(){
-
-return 0;
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array" << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements in the array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // Sorting the array::
+    for (int i = 0; i <= n - 2; i++)
+    {
+        for (int j = i; j <= n - 1; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+    cout << "THE SORTED ARRAY IS ::" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
 }
